3aecd5f76d9b44f50f2fdab78115b4d18f63ef1e
tamas <ruszkait@gmail.com>
2018-03-06 12:55:53 +0100

    Emission moved to Registration

--- CppSignal/CppSignal.h
+++ CppSignal/CppSignal.h
@@ -56,6 +56,8 @@ namespace CppSignal
 		public:
 			void Unsubscribe() override;
 
+			void Emit(TSignalParameters ... signalParameters);
+
 			Callback _callback;
 		};
 
@@ -92,6 +94,11 @@ namespace CppSignal
 // ==========================================================================================================================
 
 	template<typename ...TSignalParameters>
+	Signal<TSignalParameters...>::Signal(int numberOfMaxRegistrations)
+		: _registrations(numberOfMaxRegistrations)
+	{}
+
+	template<typename ...TSignalParameters>
 	template<typename TPublisher>
 	Subscription Signal<TSignalParameters...>::Subscribe(const std::shared_ptr<TPublisher>& publisher, const Callback& callback)
 	{
@@ -113,54 +120,11 @@ namespace CppSignal
 	}
 
 	template<typename ...TSignalParameters>
-	Signal<TSignalParameters...>::Signal(int numberOfMaxRegistrations)
-		: _registrations(numberOfMaxRegistrations)
-	{}
-
-	template<typename ...TSignalParameters>
 	void Signal<TSignalParameters...>::Emit(TSignalParameters ... signalParameters)
 	{
 		for (auto& registration : _registrations)
 		{
-			RegistrationStatus lastKnownCurrentRegistrationStatus;
-
-			bool transitionSucceeded;
-			while(true)
-			{
-				lastKnownCurrentRegistrationStatus = RegistrationStatus::Used;
-				transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Emitting);
-				if (transitionSucceeded)
-					break;
-
-				auto anEmissionIsAlreadyRunningFromAnotherThread = lastKnownCurrentRegistrationStatus == RegistrationStatus::Emitting;
-				if (anEmissionIsAlreadyRunningFromAnotherThread)
-				{
-					std::this_thread::sleep_for(std::chrono::milliseconds(1));
-					continue;
-				}
-
-				break;
-			}
-
-			if (!transitionSucceeded)
-				continue;
-
-			registration._callback(std::forward<TSignalParameters ...>(signalParameters)...);
-
-			lastKnownCurrentRegistrationStatus = RegistrationStatus::Emitting;
-			transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Used);
-
-			auto registrationWasDestroyedDuringEmission = !transitionSucceeded;
-			if (registrationWasDestroyedDuringEmission)
-			{
-				lastKnownCurrentRegistrationStatus = registration._status;
-				assert(lastKnownCurrentRegistrationStatus == RegistrationStatus::PendingDestruction);
-
-				registration._callback = nullptr;
-
-				transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Empty);
-				assert(transitionSucceeded);
-			}
+			registration.Emit(std::forward<TSignalParameters ...>(signalParameters)...);
 		}
 	}
 
@@ -169,10 +133,7 @@ namespace CppSignal
 	{
 		for (auto& registration : _registrations)
 		{
-			RegistrationStatus lastKnownCurrentRegistrationStatus = registration._status;
-			if (lastKnownCurrentRegistrationStatus != RegistrationStatus::Empty)
-				continue;
-
+			auto lastKnownCurrentRegistrationStatus = RegistrationStatus::Empty;
 			auto transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Populating);
 			if (!transitionSucceeded)
 				continue;
@@ -195,18 +156,15 @@ namespace CppSignal
 	{
 		for (auto& registration : _registrations)
 		{
-			RegistrationStatus lastKnownCurrentrRegistrationStatus = registration._status;
-			if (lastKnownCurrentrRegistrationStatus != RegistrationStatus::Empty)
-				continue;
-
-			auto transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentrRegistrationStatus, RegistrationStatus::Populating);
+			auto lastKnownCurrentRegistrationStatus = RegistrationStatus::Empty;
+			auto transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Populating);
 			if (!transitionSucceeded)
 				continue;
 
 			registration._callback = std::move(callback);
 
-			lastKnownCurrentrRegistrationStatus = RegistrationStatus::Populating;
-			transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentrRegistrationStatus, RegistrationStatus::Used);
+			lastKnownCurrentRegistrationStatus = RegistrationStatus::Populating;
+			transitionSucceeded = registration._status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Used);
 			assert(transitionSucceeded);
 
 			return registration;
@@ -215,6 +173,8 @@ namespace CppSignal
 		// No free registration left
 		throw std::bad_alloc();
 	}
+
+	// ==========================================================================================================================
 
 	template<typename ...TSignalParameters>
 	void Signal<TSignalParameters...>::Registration::Unsubscribe()
@@ -246,4 +206,46 @@ namespace CppSignal
 			}
 		}
 	}
+	
+	template<typename ...TSignalParameters>
+	void Signal<TSignalParameters...>::Registration::Emit(TSignalParameters ... signalParameters)
+	{
+		RegistrationStatus lastKnownCurrentRegistrationStatus;
+
+		bool transitionSucceeded;
+		while (true)
+		{
+			lastKnownCurrentRegistrationStatus = RegistrationStatus::Used;
+			transitionSucceeded = _status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Emitting);
+			if (transitionSucceeded)
+				break;
+
+			auto anEmissionIsAlreadyRunningFromAnotherThread = lastKnownCurrentRegistrationStatus == RegistrationStatus::Emitting;
+			if (anEmissionIsAlreadyRunningFromAnotherThread)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				continue;
+			}
+
+			return;
+		}
+
+		_callback(std::forward<TSignalParameters ...>(signalParameters)...);
+
+		lastKnownCurrentRegistrationStatus = RegistrationStatus::Emitting;
+		transitionSucceeded = _status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Used);
+
+		auto registrationWasDestroyedDuringEmission = !transitionSucceeded;
+		if (registrationWasDestroyedDuringEmission)
+		{
+			lastKnownCurrentRegistrationStatus = _status;
+			assert(lastKnownCurrentRegistrationStatus == RegistrationStatus::PendingDestruction);
+
+			_callback = nullptr;
+
+			transitionSucceeded = _status.compare_exchange_strong(lastKnownCurrentRegistrationStatus, RegistrationStatus::Empty);
+			assert(transitionSucceeded);
+		}
+	}
+
 }
